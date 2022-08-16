f1cf545d75d018e096f932f0d8727e9ed6b72dc6
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-08-16 13:57:40 +0900

    navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 import images from "../../constants/images";
@@ -8,6 +8,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   background-color: #000;
+  position: relative;
 `;
 
 export const Wrapper = styled.div`
@@ -18,7 +19,7 @@ export const Wrapper = styled.div`
 `;
 
 export const ImgBox = styled.div`
-  width: 10%;
+  width: 8rem;
 `;
 
 export const Image = styled.img`
@@ -39,7 +40,7 @@ export const Nav = styled.ul`
     color: #fff;
   }
 
-  @media screen and (max-width: 900px) {
+  @media screen and (max-width: 950px) {
     display: none;
   }
 `;
@@ -49,81 +50,156 @@ export const Menu = styled.div`
   display: flex;
   align-items: center;
 
-  & div {
-    margin: 0 1.4rem;
-    height: 2rem;
-    width: 1px;
-    background-color: #fff;
-    opacity: 0.5;
-  }
-
   /* svg가 요소 중간에 떠있다. 왜 그럴까? */
   & svg {
-    margin: 0 2rem;
+    margin: 0rem;
     font-size: 2rem;
     display: none;
+    z-index: 3;
+    cursor: pointer;
+
+    @media screen and (max-width: 950px) {
+      display: block;
+    }
   }
 
-  & section {
-    display: none;
+  & .close {
+    position: absolute;
+    top: 3%;
+    right: 4%;
+  }
+
+  @media screen and (max-width: 950px) {
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
   }
 `;
 
-const Navbar = () => (
-  <Container>
-    <Wrapper>
-      {/* 로고 사이드 */}
-      <ImgBox>
-        <Image src={images.gericht} alt="" className="logo" />
-      </ImgBox>
+export const Right = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
 
-      {/* 네비게이션 사이드 */}
-      <Nav>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Pages</a>
-        </li>
-        <li>
-          <a href="#">Contact Us</a>
-        </li>
-        <li>
-          <a href="#">Blog</a>
-        </li>
-        <li>
-          <a href="#">Landing</a>
-        </li>
-      </Nav>
+export const Line = styled.div`
+  margin: 0 1.4rem;
+  height: 2rem;
+  width: 1px;
+  background-color: #fff;
+  opacity: 0.5;
+`;
 
-      {/* 로그인 & 회원가입 사드 */}
-      <Menu>
-        <p>Log in / Registeration</p>
-        <div></div>
-        <p>Book Table</p>
-        <section>
-          <GiHamburgerMenu />
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Pages</a>
-            </li>
-            <li>
-              <a href="#">Contact Us</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-            <li>
-              <a href="#">Landing</a>
-            </li>
-          </ul>
-        </section>
-      </Menu>
-    </Wrapper>
-  </Container>
-);
+export const Overlay = styled.section`
+  display: none;
+
+  @media screen and (max-width: 950px) {
+    display: flex;
+  }
+`;
+
+export const OverlayList = styled.ul`
+  display: none;
+  list-style: none;
+
+  @media screen and (max-width: 950px) {
+    display: ${(props) => (props.toggle ? "flex" : "none")};
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: #000;
+    z-index: 3;
+
+    & li {
+      margin: 1rem 0;
+      color: #dcca87;
+      font-weight: 700;
+      font-size: 1.6rem;
+      transition: 0.5s;
+
+      &:hover {
+        color: #fff;
+      }
+    }
+  }
+`;
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <Container>
+      <Wrapper>
+        {/* 로고 사이드 */}
+        <ImgBox>
+          <Image src={images.gericht} alt="" className="logo" />
+        </ImgBox>
+
+        {/* 네비게이션 사이드 */}
+        <Nav>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Pages</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Landing</a>
+          </li>
+        </Nav>
+
+        {/* 로그인 & 회원가입 사드 */}
+        <Menu>
+          <Right>
+            <p>Log in / Registeration</p>
+            <Line></Line>
+            <p>Book Table</p>
+          </Right>
+
+          <Overlay>
+            {!toggle && <GiHamburgerMenu onClick={() => setToggle(true)} />}
+
+            <OverlayList toggle={toggle} className="slide-right">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Pages</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Landing</a>
+              </li>
+
+              {toggle && (
+                <MdOutlineRestaurantMenu
+                  onClick={() => setToggle(false)}
+                  className="close"
+                />
+              )}
+            </OverlayList>
+          </Overlay>
+        </Menu>
+      </Wrapper>
+    </Container>
+  );
+};
 
 export default Navbar;
