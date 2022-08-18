3a8991492441fb629f683581185ff84cde4f247b
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-08-18 10:33:41 +0900

    completed specialmenu

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -10,10 +10,16 @@ const MenuItem = ({ title, price, tags }) => (
           {title}
         </p>
       </div>
-
+      <div className="app__menuitem-dash" />
       <div className="app__menuitem-price">
         <p className="p__cormorant">{price}</p>
       </div>
+    </div>
+
+    <div className="app__menuitem-sub">
+      <p className="p__opensans" style={{ color: "#AAAAAA" }}>
+        {tags}
+      </p>
     </div>
   </div>
 );
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -6,14 +6,13 @@ import "./SpecialMenu.css";
 const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
-      <SubHeading title="Menu that fits you palatte" />
-      <h1 className="headtext__cormorant">Today's Special</h1>
+      <SubHeading title="Menu that fits your palatte" />
+      <h1 className="headtext__cormorant">Today&apos;s Special</h1>
     </div>
 
     <div className="app__specialMenu-menu">
-      <div className="app__specialMenu-menu_wine flex__center">
-        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
-
+      <div className="app__specialMenu-menu_wine  flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
             <MenuItem
@@ -27,12 +26,11 @@ const SpecialMenu = () => (
       </div>
 
       <div className="app__specialMenu-menu_img">
-        <img src={images.menu} alt="menu-img" />
+        <img src={images.menu} alt="menu__img" />
       </div>
 
-      <div className="app__specialMenu-menu_cocktails flex__center">
-        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
-
+      <div className="app__specialMenu-menu_cocktails  flex__center">
+        <p className="app__specialMenu-menu_heading">Cocktails</p>
         <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
             <MenuItem
@@ -46,7 +44,7 @@ const SpecialMenu = () => (
       </div>
     </div>
 
-    <div style={{ marginTop: "15px" }}>
+    <div style={{ marginTop: 15 }}>
       <button type="button" className="custom__button">
         View More
       </button>
